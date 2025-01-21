39344f64bbc5f37a1a87aa0b9ba1ef03f8cde893
lucymary <lucymary.am@gmail.com>
2025-01-21 14:27:23 -0400

    Endpoints modificados para retornar JSON

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Usuario;
 use Illuminate\Support\Facades\Hash; 
+use Illuminate\Support\Facades\Validator;
 
 class UsuarioController extends Controller
 {
@@ -21,22 +22,27 @@ class UsuarioController extends Controller
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
+        $validator = Validator::make($request->all(), [
             'nome' => 'required|string|min:3|max:50',
             'email' => 'required|email|unique:usuarios,email',
             'senha' => 'required|string|min:6|max:20|confirmed', 
         ], [
             'email.email' => 'Informe um e-mail válido',
             'email.unique' => 'Este e-mail já está em uso. Por favor, escolha outro.',
-        ]);
+        ])->stopOnFirstFailure(true);
+
+        $data = $validator->validate();
 
         $user = new Usuario();
-        $user->nome = $validatedData['nome'];
-        $user->email = $validatedData['email'];
-        $user->senha = Hash::make($validatedData['senha']); 
+        $user->nome = $data['nome'];
+        $user->email = $data['email'];
+        $user->senha = Hash::make($data['senha']); 
         $user->save();
-
-        return redirect()->route('usuarios.index')->with('success', 'Usuário cadastrado com sucesso!');;
+      
+        return response()->json([
+            'success' => true,
+            'message' => 'Usuário cadastrado com sucesso!',
+        ], 200);
     }
 
     public function edit($id)
@@ -47,18 +53,23 @@ class UsuarioController extends Controller
 
     public function update(Request $request, $id)
     {
-        $validatedData = $request->validate([
+        $validator = Validator::make($request->all(), [
             'nome' => 'required|string|min:3|max:50',
             'email' => 'required|email|unique:usuarios,email,' . $id,           
         ], [
             'email.email' => 'Informe um e-mail válido',
             'email.unique' => 'Este e-mail já está em uso. Por favor, escolha outro.',
-        ]);
-        
-        $usuario = Usuario::findOrFail($id);
-        $usuario->update($validatedData);
+        ])->stopOnFirstFailure(true);
 
-        return redirect()->route('usuarios.index')->with('success', 'Usuário atualizado com sucesso!');;
+        $data = $validator->validate();
+
+        $usuario = Usuario::findOrFail($id);
+        $usuario->update($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Usuário alterado com sucesso!',
+        ], 200);
     }
 
     public function delete($id)
